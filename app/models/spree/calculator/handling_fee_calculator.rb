18830090f843159a2a3ed88f8cb3602c5f730487
dfreerksen <dfreerksen@gmail.com>
2016-05-17 17:13:36 -0700

    Correct the handling fee calculation for shipping

--- app/models/spree/calculator/handling_fee_calculator.rb
+++ app/models/spree/calculator/handling_fee_calculator.rb
@@ -4,19 +4,18 @@ module Spree
       Spree.t("handling_fees.calculator_description")
     end
 
+    # This is only here for Spree < 2.2. Spree 2.2+ calculates on shipment and
+    # line items rather than order
     def compute_order(_order)
       raise "Spree::Calculator::HandlingFeeCalculator is designed to " \
             "calculate taxes at the line-item level."
     end
 
-    def compute_shipment(_shipment)
-      raise "Spree::Calculator::HandlingFeeCalculator is designed to " \
-            "calculate taxes at the line-item level."
-    end
-
-    def compute_line_item(item)
+    def compute_shipment_or_line_item(item)
       round_to_two_places calculate_handling_fee_for(item)
     end
+    alias_method :compute_shipment, :compute_shipment_or_line_item
+    alias_method :compute_line_item, :compute_shipment_or_line_item
 
     private
 
